dbe06f939a689df1300939e8e6900f1fdafc10b1
rubiaek <ronen.shekel@gmail.com>
2025-02-06 13:16:50 +0200

    foo

--- qdc/qdc_result.py
+++ qdc/qdc_result.py
@@ -1,7 +1,18 @@
+import copy
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 class QDCResult(object):
     def __init__(self):
-        pass
+        self.classical_PCCs = None
+        self.two_ph_PCCs = None
+
+
+    def saveto(self, path):
+        d = copy.deepcopy(self.__dict__)
+        np.savez(path, **d)
+
+    def loadfrom(self, path):
+        data = np.load(path, allow_pickle=True)
+        self.__dict__.update(data)
