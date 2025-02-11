02f1737d8c33bfd3a6927030a02233fc2af673c0
rubiaek <ronen.shekel@gmail.com>
2025-02-11 10:52:22 +0200

    switched to a 2f configuration, using a thin lens phase mask

--- qdc/diffuser/diffuser_result.py
+++ qdc/diffuser/diffuser_result.py
@@ -1,11 +1,21 @@
+from qdc.diffuser.field import Field
 import numpy as np
+import matplotlib.pyplot as plt
 import copy
+
 
 class DiffuserResult:
     def __init__(self):
-        self.delta_lambdas = None
-        self.fields = None
-        self.final_wls = None
+        self._SPDC_fields_E = None
+        self._SPDC_fields_wl = None
+        self.SPDC_fields = []
+        self.SPDC_delta_lambdas = None
+        self.wavelengths = None
+
+
+    def _populate_fields(self):
+        for field_E, wl in zip(self._SPDC_fields_E, self._SPDC_fields_wl):
+            self.SPDC_fields.append(Field(self.x, self.y, wl, field_E))
 
     def compute_contrast(self, roi=None):
         """
@@ -18,6 +28,24 @@ class DiffuserResult:
         else:
             data = self.intensity_map
         return np.std(data) / np.mean(data)
+
+    def plot_SPDC_PCCs(self):
+        fig, ax = plt.subplots()
+        PCCs = []
+        f0 = self.SPDC_fields[0]
+        for f in self.SPDC_fields:
+            PCC = np.corrcoef(f0.I.ravel(), f.I.ravel())[0, 1]
+            PCCs.append(PCC)
+        ax.plot(self.SPDC_delta_lambdas*1e9, PCCs, '*--', label='PCC')
+        ax.set_xlabel('$\Delta\lambda$ [nm]')
+        ax.set_ylabel('PCC')
+        fig.show()
+
+    def show_incoherent_sum_SPDC(self):
+        final_I = np.zeros_like(self.SPDC_fields[0].I)
+        for field in self.SPDC_fields:
+            final_I += field.I
+        Field(self.x, self.y, self.wavelengths[0], np.sqrt(final_I)).show(title='Incoherent sum of all wavelengths')
 
     def saveto(self, path):
         d = copy.deepcopy(self.__dict__)
--- qdc/diffuser/diffuser_sim.py
+++ qdc/diffuser/diffuser_sim.py
@@ -9,22 +9,25 @@ class DiffuserSimulation:
     def __init__(self,
                  Nx=512, Ny=512, Lx=2e-3, Ly=2e-3,
                  wl0=808e-9, Dwl=40e-9, N_wl=41,
-                 waist=20e-6, distance_to_det=100e-3,
-                 init_off_axis=200e-6, diffuser_angle=0.5):
+                 waist=20e-6, focal_length=100e-3,
+                 init_off_axis=200e-6, diffuser_angle=0.5, achromat_lens=True):
 
         self.res = DiffuserResult()
         self.res.Nx = Nx
         self.res.Ny = Ny
         self.res.x = np.linspace(-Lx/2, Lx/2, Nx)
         self.res.y = np.linspace(-Ly/2, Ly/2, Ny)
+        self.XX, self.YY = np.meshgrid(self.x, self.y)
         self.res.wl0 = wl0
         self.res.Dwl = Dwl
         self.res.N_wl = N_wl
         self.res.waist = waist
-        self.res.dz = distance_to_det
+        self.res.f = focal_length
         self.res.init_off_axis = init_off_axis
         self.res.diffuser_angle = diffuser_angle
         self.res.wavelengths = self._get_wl_range()
+        self.res.diffuser_mask = phase_screen_diff(self.x, self.y, self.wl0, self.diffuser_angle)
+        self.res.achromat_lens = achromat_lens
 
 
     def __getattr__(self, name):
@@ -47,13 +50,20 @@ class DiffuserSimulation:
 
     def make_detection_gaussian(self, lam):
         """ Make a Gaussian beam at the detection plane with self.waist. """
-        XX, YY = np.meshgrid(self.x, self.y)
-        r2 = (XX-self.init_off_axis)**2 + YY**2
+        r2 = (self.XX-self.init_off_axis)**2 + self.YY**2
         E = np.exp(-r2 / (self.waist**2), dtype=np.complex128)
         E /= np.sqrt((np.abs(E)**2).sum())
         return Field(self.x, self.y, lam, E)
 
-    def run_SPDC_simulation(self) -> (list[float], list[Field]):
+
+    def get_lens_mask(self, f, wl):
+        k = 2 * np.pi / wl
+        # important -i, assuming freespace is with +i
+        mask = np.exp(-1j * (self.XX ** 2 + self.YY ** 2) * k / (2 * f))
+        return mask
+
+
+    def run_SPDC_simulation(self):
         """ returns list of output fields and one-sided delta lambdas"""
         i_middle = self.N_wl // 2
         # number of measurements: degenerate + pairs
@@ -62,15 +72,18 @@ class DiffuserSimulation:
         delta_lambdas = []
 
         wl0 = self.wavelengths[i_middle]
-        diffuser_mask = phase_screen_diff(self.x, self.y, wl0, self.diffuser_angle)
 
         # degenerate forward pass
         field_det = self.make_detection_gaussian(wl0)
-        field_crystal = propagate_free_space(field_det, self.dz)
-        field_crystal.E *= np.exp(1j * diffuser_mask)
+        field_lens = propagate_free_space(field_det, self.f)
+        field_lens.E *= self.get_lens_mask(self.f, wl0)
+        field_crystal = propagate_free_space(field_lens, self.f)
+        field_crystal.E *= np.exp(1j * self.diffuser_mask)
         # here "switch wl", but degenerate
-        field_crystal.E *= np.exp(1j * diffuser_mask)
-        field_det_new = propagate_free_space(field_crystal, self.dz)
+        field_crystal.E *= np.exp(1j * self.diffuser_mask)
+        field_lens2 = propagate_free_space(field_crystal, self.f)
+        field_lens2.E *= self.get_lens_mask(self.f, wl0)
+        field_det_new = propagate_free_space(field_lens2, self.f)
 
         delta_lambdas.append(0.0)
         fields.append(field_det_new)
@@ -81,14 +94,25 @@ class DiffuserSimulation:
             wl_minus = self.wavelengths[i_middle - di]
 
             field_det = self.make_detection_gaussian(wl_plus)
-            field_crystal = propagate_free_space(field_det, self.dz)
-            field_crystal.E *= np.exp(1j * diffuser_mask*self.wl0/wl_plus)
+            field_lens = propagate_free_space(field_det, self.f)
+            field_lens.E *= self.get_lens_mask(self.f, wl_plus if self.achromat_lens else wl0)
+            field_crystal = propagate_free_space(field_lens, self.f)
+            # width of diffuser works differently for different wavelengths,
+            # and assuming a very thin diffuser that does only 2pi for wl0
+            field_crystal.E *= np.exp(1j * self.diffuser_mask*self.wl0/wl_plus)
 
+            # change wl at crystal (could potentially add phase matching etc.)
             field_crystal.wl = wl_minus
-            field_crystal.E *= np.exp(1j * diffuser_mask*self.wl0/wl_minus)
-            field_det_new = propagate_free_space(field_crystal, self.dz)
 
+            field_crystal.E *= np.exp(1j * self.diffuser_mask*self.wl0/wl_minus)
+            field_lens2 = propagate_free_space(field_crystal, self.f)
+            field_lens2.E *= self.get_lens_mask(self.f, wl_minus if self.achromat_lens else wl0)
+            field_det_new = propagate_free_space(field_lens2, self.f)
             delta_lambdas.append(wl_plus - wl_minus)
             fields.append(field_det_new)
 
-        return delta_lambdas, fields
+        self.res._SPDC_fields_E = np.array([f.E for f in fields])
+        self.res._SPDC_fields_wl = np.array([f.wl for f in fields])
+        self.res.SPDC_delta_lambdas = np.array(delta_lambdas)
+        self.res._populate_fields()
+        return self.res
